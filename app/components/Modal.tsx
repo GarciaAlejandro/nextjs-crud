8c871b1d8142a46c059cd6616acacf960c7972d2
GarciaAlejandro <garciacetinaalejandro@gmail.com>
2024-04-26 10:29:10 -0600

    chore: Update Modal component styles to use a semi-transparent background

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Modal = ({ children, onClose }: { children: React.ReactNode, onClose: () => void }) => {
   return (
-    <div className="modal-custom row justify-content-center container-fluid" style={{background: 'rgba(0,0,0,0.5)'}}>
+    <div className="modal-custom row justify-content-center container-fluid " style={{background: 'rgba(0,0,0,0.25)', width: '100%'}}>
       <div className="card col-7">
         <div className="modal-content pa-5">
             <span onClick={onClose} style={{cursor: 'pointer', textAlign: 'right'}}>&times;</span>
